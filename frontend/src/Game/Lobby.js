8f257b922faeebe601a66aca92e3a2bfc04d5dce
Hans Sebastian Tirtaputra <sebastian_hans@hotmail.com>
2023-01-15 02:41:13 +0800

    add lobby game grid

--- frontend/src/Game/Lobby.js
+++ frontend/src/Game/Lobby.js
@@ -1,18 +1,85 @@
 import * as React from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
+import TextField from "@mui/material/TextField"
+import Container from "@mui/material/Container"
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
-export default function Lobby() {
+const userID = "test"
+
+const GameGrid = ({ words }) => {
+  const getCardColor = (wordObj) => {
+    const capturedBy = wordObj['capturedBy']
+
+    if (capturedBy === "") {
+      return {}
+    }
+
+    return capturedBy === userID
+      ? {backgroundColor:"#7FFFD4"}
+      : {backgroundColor:"#A52A2A"}
+  }
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {Array.from(Array(25)).map((_, index) => (
-          <Grid item xs={2} sm={4} md={4} key={index}>
-            <Paper variant="outlined" />
+    <Box sx={{ flexGrow: 1 }} mt={8}>
+      <Grid container spacing={3} >
+        {words.map((wordObj, idx) => (
+          <Grid item md={3} key={idx}>
+            <Paper variant="outlined" style={getCardColor(wordObj)}>
+              {wordObj['word']}
+            </Paper>
           </Grid>
         ))}
       </Grid>
     </Box>
-  );
+  )
+}
+
+const AnswerInputBox = () => {
+  const [answer, setAnswer] = React.useState('');
+
+  const handleChange = (event) => {
+    setAnswer(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    // TODO: Send word through socket to server here, get response and update the GameGrid
+    if (event.key !== 'Enter') {
+      alert(answer);
+    }
+  }
+
+  return (
+    <TextField 
+      id="outlined-basic" 
+      label="Input Word" 
+      variant="outlined"
+      fullWidth
+      margin="normal"
+      value={answer}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
+    />
+  )
+}
+
+export default function Lobby() {
+  const [words, setWords] = useState([
+    {word: "fluffy", capturedBy: ""}, {word: "boy", capturedBy: ""}, {word: "text", capturedBy: ""}, {word: "example", capturedBy: ""},
+    {word: "fluffy1", capturedBy: "test"}, {word: "boy1", capturedBy: ""}, {word: "text1", capturedBy: ""}, {word: "example1", capturedBy: ""},
+    {word: "fluffy2", capturedBy: "otherID"}, {word: "boy2", capturedBy: ""}, {word: "text2", capturedBy: ""}, {word: "example2", capturedBy: ""},
+    {word: "fluffy3", capturedBy: ""}, {word: "boy3", capturedBy: ""}, {word: "text3", capturedBy: ""}, {word: "example3", capturedBy: ""},
+    {word: "fluffy4", capturedBy: ""}, {word: "boy4", capturedBy: ""}, {word: "text4", capturedBy: ""}, {word: "example4", capturedBy: ""},
+    {word: "fluffy5", capturedBy: ""}, {word: "boy5", capturedBy: ""}, {word: "text5", capturedBy: ""}, {word: "example5", capturedBy: ""},
+  ])
+
+  // TODO: Setup socket listener to update words
+
+  return (
+    <Container>
+      <GameGrid words={words}/>
+      <AnswerInputBox/>
+    </Container>
+  )
 }
--- frontend/src/Home/CreateGame.js
+++ frontend/src/Home/CreateGame.js
@@ -1,13 +1,4 @@
 import Button from "@mui/material/Button";
-const { io } = require("socket.io-client");
-
-const socket = io();
-
-initializeConnection = () => {
-    socket.emit("connect", (s) => {
-        
-    })
-}
 
 function CreateGame() {
     return <Button variant="contained">Create Game</Button>;
